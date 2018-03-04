87745ab335dabe8c7dcfa5f04893945be3685fe4
Kostis Kapelonis <kkapelon@gmail.com>
2018-03-04 03:56:09 -0500

    Added automation script and sample pictures

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -d "bitnami-docker-wordpress" ]; then
+	git clone https://github.com/bitnami/bitnami-docker-wordpress.git
+fi
+cd bitnami-docker-wordpress
+docker-compose up -d
+cd ..
+sleep 30
+chmod +x ./wait-for-it.sh
+./wait-for-it.sh -t 30 localhost:80 -- echo "wordpress is up"
+cd geb-tester
+./gradlew test
+cd ..
+cd bitnami-docker-wordpress
+docker-compose down
